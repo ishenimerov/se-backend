2cbc0ae2aa0065be4c1a6da02f75174f1348cfa2
Ishen Imerov <macbook_pro@MacBook-Pro-MacBook-Pro.local>
2024-06-02 01:13:19 +0200

    refactored, filters added

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', useRoutes);
-console.log('s');
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
